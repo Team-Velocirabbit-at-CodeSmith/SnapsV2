dbeb0a590242177e0d5cbb43809627b300421b9b
erikawester <erikarwester@gmail.com>
2024-01-11 15:20:36 -0500

    set up successful testing for userController sanitize method

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,28 @@
-const db = require('../models/snapsModel');
-
+const db = require("../models/snapsModel");
 
 const userController = {};
 
+const sanitize = (unsanitizedString) => {
+  return unsanitizedString.trim();
+};
+
 userController.login = async (req, res, next) => {
-  console.log('inside login controller!');
+  console.log("inside login controller!");
   console.log(
-    'this is req.params stuff',
+    "this is req.params stuff",
     req.params.username,
     req.params.password
   );
   try {
+    //first sanitize the inputted string
+    // userController.sanitize = (unsanitizedString) => {
+    //   return unsanitizedString.trim();
+    // };
+
+    sanitize(req.params.username);
+
     const queryObj = {
-      text: 'SELECT Users.id, Snaps.snap_id, Snaps.title, Snaps.url, Snaps.snap_text FROM Users LEFT OUTER JOIN Snaps ON Users.id = Snaps.user_id WHERE Users.username = $1 AND Users.password = $2;',
+      text: "SELECT Users.id, Snaps.snap_id, Snaps.title, Snaps.url, Snaps.snap_text FROM Users LEFT OUTER JOIN Snaps ON Users.id = Snaps.user_id WHERE Users.username = $1 AND Users.password = $2;",
       values: [req.params.username, req.params.password],
     };
     const user = await db.query(queryObj);
@@ -22,9 +32,9 @@ userController.login = async (req, res, next) => {
     return next();
   } catch {
     const err = {
-      log: 'Express error handler caught error in userController.login',
+      log: "Express error handler caught error in userController.login",
       status: 500,
-      message: { err: 'A massive error occured' + JSON.stringify(process.env) },
+      message: { err: "A massive error occured" + JSON.stringify(process.env) },
     };
     return next(err);
   }
@@ -33,22 +43,23 @@ userController.login = async (req, res, next) => {
 userController.signup = async (req, res, next) => {
   try {
     const queryObj = {
-      text: 'INSERT INTO Users (username, password) VALUES ($1, $2)',
+      text: "INSERT INTO Users (username, password) VALUES ($1, $2)",
       values: [req.body.username, req.body.password],
     };
     const newUser = await db.query(queryObj);
-    console.log(newUser, 'newuser');
+    console.log(newUser, "newuser");
     res.locals.newUser = newUser.rows;
     // console.log('this is user', user);
     return next();
   } catch {
     const err = {
-      log: 'Express error handler caught error in userController.signup',
+      log: "Express error handler caught error in userController.signup",
       status: 500,
-      message: { err: 'A massive error occured' },
+      message: { err: "A massive error occured" },
     };
     return next(err);
   }
 };
 
 module.exports = userController;
+module.exports = sanitize;
